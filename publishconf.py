0ae1ff7eae80dca879ada53137952756aaa7d337
Priyanshu Bishnoi <103980769+priyanshubishnoi@users.noreply.github.com>
2025-09-15 00:48:23 +0530

    theme

--- publishconf.py
+++ publishconf.py
@@ -10,12 +10,17 @@ from pelicanconf import *
 # If your site is available via HTTPS, make sure SITEURL begins with https://
 SITEURL = "https://priyanshubishnoi.github.io/tech-blog"
 RELATIVE_URLS = False
-
+THEME = "themes/papyrus"
 FEED_ALL_ATOM = "feeds/all.atom.xml"
 CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
 
 DELETE_OUTPUT_DIRECTORY = True
-
+PLUGIN_PATHS = ['pelican-plugins']
+PLUGINS = [
+    'pelican_ga4',
+    # ... other plugins you use
+] # adjust path
+GA4_MEASUREMENT_ID = "G-98GLR120T4"
 # Following items are often useful when publishing
 
 # DISQUS_SITENAME = ""
